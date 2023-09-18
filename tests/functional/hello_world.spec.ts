ebad04ec6609394eee958a3b2ff3c62ec7b223fe
Mily Moreno <milydemendoza@gmail.com>
2023-09-18 13:49:40 -0400

    testeando lista de pacientes

--- tests/functional/hello_world.spec.ts
+++ tests/functional/hello_world.spec.ts
@@ -1,8 +1,8 @@
 import { test } from '@japa/runner'
 
 test('display welcome page', async ({ client }) => {
-  const response = await client.get('/')
+  const response = await client.get('/patients')
 
   response.assertStatus(200)
-  response.assertBodyContains({ hello: 'world' })
+  // response.assertBodyContains({ hello: 'world' })
 })
